40509f00804085f6cf8d8ccec21b6788330fa540
Gabriel Fontes <gabrielferroroque@hotmail.com>
2021-05-29 00:11:52 -0300

    [CONFIG] Added CORS initialize file.

---
+++ config/initializers/cors.rb
@@ -0,0 +1,13 @@
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins 'http://localhost:3000'
+    resource '*',
+             headers: :any,
+             expose: %w[access-token expiry token-type uid client],
+             methods: %i[get post put patch delete options head],
+             credentials: true
+  end
+end
+# Rails.application.config.hosts.clear
+# Rails.application.config.hosts = nil
+# debug: true, logger: (-> { Rails.logger })
